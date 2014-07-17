d09ac609068063bf42598d8cdf1b0c30dfa8ec46
kenjis <kenji.uui@gmail.com>
2014-07-17 20:56:44 +0900

    AspectMockのインストール

---
+++ fuel/app/tests/controller/form_Test.php
@@ -0,0 +1,141 @@
+<?php
+
+// AspectMock\Testクラスをtestとしてインポート
+use AspectMock\Test as test;
+
+/**
+ * Controller Form class Tests
+ * 
+ * @group App
+ * @group AppController
+ */
+class controller_form_Test extends AmTestCase
+{
+	public function test_indexアクションをGETするとコンタクトフォームページが返る()
+	{
+		// HMVCリクエストを生成
+		$request = Request::forge('form/index');
+		// リクエストを実行しレスポンスを取得
+		$response = $request->execute()->response();
+		
+		// HTTPステータスコードのテスト
+		$status = $response->status;
+		$expected = 200;
+		$this->assertEquals($expected, $status);
+		
+		// titleのテスト
+		$title = $response->body->title;
+		$expected = 'コンタクトフォーム';
+		$this->assertEquals($expected, $title);
+		
+		// HTMLのテスト
+		$body = $response->body()->render();
+		$pattern = '/' . preg_quote('お問い合わせ', '/') . '/u';
+		$this->assertRegExp($pattern, $body);
+	}
+	
+	public function test_indexアクションにPOSTするとFieldsetのrepopulateメソッドが実行される()
+	{
+		// Input::method()を単にPOSTを返すモックに置き換え
+		$input = test::double('Fuel\Core\Input', ['method' => 'POST']);
+		// Fieldset::repopulate()を単にtrueを返すモックに置き換え
+		$fs = test::double('Fuel\Core\Fieldset', ['repopulate' => true]);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/index')->execute()->response();
+		
+		// Fieldset::repopulate()が実行されたことを確認
+		$fs->verifyInvoked('repopulate');
+	}
+	
+	public function test_confirmアクションで検証パスすると確認ページが返る()
+	{
+		// Validation::run()を単にtrueを返すモックに置き換え
+		$val = test::double('Fuel\Core\Validation', ['run' => true]);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/confirm')->execute()->response();
+		
+		// titleのテスト
+		$title = $response->body->title;
+		$expected = 'コンタクトフォーム: 確認';
+		$this->assertEquals($expected, $title);
+	}
+	
+	public function test_confirmアクションで検証エラーだとタイトルがエラーになる()
+	{
+		// Validation::run()を単にfalseを返すモックに置き換え
+		$val = test::double('Fuel\Core\Validation', ['run' => false]);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/confirm')->execute()->response();
+		
+		// titleのテスト
+		$title = $response->body->title;
+		$expected = 'コンタクトフォーム: エラー';
+		$this->assertEquals($expected, $title);
+	}
+	
+	/**
+	 * @expectedException HttpInvalidInputException
+	 * @expectedExceptionMessage ページ遷移が正しくありません
+	 */
+	public function test_sendアクションでCSRF検証エラーだと例外が発生()
+	{
+		// Security::check_token()を単にfalseを返すモックに置き換え
+		$sec = test::double('Fuel\Core\Security', ['check_token' => false]);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/send')->execute()->response();
+	}
+	
+	public function test_sendアクションですべてパスすると送信完了ページが返る()
+	{
+		// Security::check_token()を単にtrueを返すモックに置き換え
+		$sec = test::double('Fuel\Core\Security', ['check_token' => true]);
+		// Validation::run()を単にtrueを返すモックに置き換え
+		$val = test::double('Fuel\Core\Validation', ['run' => true]);
+		// Model_Form::save()を単にtrueを返すモックに置き換え
+		$model_form = test::double('Model_Form', ['save' => true]);
+		// Model_Mail::send()を単にtrueを返すモックに置き換え
+		$model_mail = test::double('Model_Mail', ['send' => true]);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/send')->execute()->response();
+		
+		// titleのテスト
+		$title = $response->body->title;
+		$expected = 'コンタクトフォーム: 送信完了';
+		$this->assertEquals($expected, $title);
+	}
+	
+	public function test_sendアクションでEmailValidationFailedExceptionが発生するとタイトルが送信エラーになる()
+	{
+		Package::load('email');
+		
+		// Security::check_token()を単にtrueを返すモックに置き換え
+		$sec = test::double('Fuel\Core\Security', ['check_token' => true]);
+		// Validation::run()を単にtrueを返すモックに置き換え
+		$val = test::double('Fuel\Core\Validation', ['run' => true]);
+		// Model_Form::save()を単にtrueを返すモックに置き換え
+		$model_form = test::double('Model_Form', ['save' => true]);
+		// Model_Mail::send()を例外を投げるモックに置き換え
+		$model_mail = test::double(
+			'Model_Mail',
+			['send' => function() { throw new EmailValidationFailedException; }]
+		);
+		
+		// HMVCリクエストを実行
+		$response = Request::forge('form/send')->execute()->response();
+		
+		// titleのテスト
+		$title = $response->body->title;
+		$expected = 'コンタクトフォーム: 送信エラー';
+		$this->assertEquals($expected, $title);
+		
+		// html_errorのテスト
+		$title = $response->body->content->html_error;
+		$expected = '<p>メールアドレスに誤りがあります。</p>';
+		$this->assertEquals($expected, $title);
+	}
+}
